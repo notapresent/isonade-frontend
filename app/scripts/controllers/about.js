0356339e2b7f8164901a6905c0a87a88c8f51aa3
notapresent <notapresent@gmail.com>
2015-06-17 20:22:59 +0300

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name isonadeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the isonadeApp
+ */
+angular.module('isonadeApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name isonadeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the isonadeApp
+ */
+angular.module('isonadeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
