62cbeb3b52fd1f4bcfd5eb3adebab0fc725c5cc9
notapresent <notapresent@gmail.com>
2015-06-18 21:22:27 +0000

    Channel controller

--- app/scripts/controllers/channel.js
+++ app/scripts/controllers/channel.js
@@ -9,9 +9,5 @@
  */
 angular.module('isonadeApp')
   .controller('ChannelCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.channels = [];
   });
